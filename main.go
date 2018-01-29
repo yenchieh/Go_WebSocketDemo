f72db99ecb484fdf1844eae477d99b55276247d6
Yen-Chieh Chen <yenchieh@nulab-inc.com>
2018-01-29 17:11:43 -0500

    handle incoming message

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 
 	"log"
 
-	"time"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -28,7 +26,7 @@ func main() {
 		handler.ServeHTTP(c.Writer, c.Request)
 	})
 
-	go testMessage()
+	go broadcastMessage()
 
 	router.Run(":8080")
 }
@@ -36,38 +34,34 @@ func main() {
 var clients = make(map[*websocket.Conn]string)
 var incomingMessage = make(chan string)
 
-func testMessage() {
-	for i := 0; ; i++ {
-
-		broadcastMessage(fmt.Sprintf("Message|%d", i))
-		time.Sleep(time.Second * 5)
-	}
-}
-
 func connect(connection *websocket.Conn) {
 	clients[connection] = fmt.Sprintf("Client-%d", len(clients))
 	fmt.Println("Client Added")
 	for {
 		var message string
 
-		if err := websocket.JSON.Receive(connection, &message); err != nil {
+		if err := websocket.Message.Receive(connection, &message); err != nil {
 			log.Println(err)
 			delete(clients, connection)
 			break
 		}
-		clients[connection] = message
-		fmt.Println("Received Message", message)
+		fmt.Printf("Received Message From %s \n - Message: %s", clients[connection], message)
 
 		incomingMessage <- message
 	}
 }
 
-func broadcastMessage(message string) {
-	for client, name := range clients {
-		if err := websocket.Message.Send(client, fmt.Sprintf("%s : %s", name, message)); err != nil {
-			log.Println(err)
-			delete(clients, client)
-			break
+func broadcastMessage() {
+	for {
+		message := <-incomingMessage
+		fmt.Println(message)
+		for client, name := range clients {
+			if err := websocket.Message.Send(client, fmt.Sprintf("%s : %s", name, message)); err != nil {
+				log.Println(err)
+				delete(clients, client)
+				break
+			}
 		}
 	}
+
 }
