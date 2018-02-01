b3ac1c703a824822680b613698e500291f1708c3
Yen-Chieh Chen <yenchieh@nulab-inc.com>
2018-02-01 16:26:00 -0500

    fix bugs

--- main.go
+++ main.go
@@ -16,29 +16,35 @@ func main() {
 
 	router.LoadHTMLGlob("view/*")
 
-	// This handler will match /user/john but will not match neither /user/ or /user
+	// Chat room page
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
+	// WebSocket
 	router.GET("/chat/ws", func(c *gin.Context) {
 		handler := websocket.Handler(connect)
 		handler.ServeHTTP(c.Writer, c.Request)
 	})
 
+	// Create goroutine to listen message channel and broadcast it to all of connection
 	go broadcastMessage()
 
 	router.Run(":8080")
 }
 
+/**
+ * Chat Room handler
+ * Client map store client number (ID) using connection as key
+ */
 var clients = make(map[*websocket.Conn]string)
 var incomingMessage = make(chan string)
 var connectionNum = 0
 
-func connect(connection *websocket.Conn) {  
+func connect(connection *websocket.Conn) {
 	connectionNum++
 	clients[connection] = fmt.Sprintf("Client-%d", connectionNum)
-	fmt.Println("Client Added")
+	fmt.Printf("\n%s Added\n", clients[connection])
 	for {
 		var message string
 
@@ -47,7 +53,7 @@ func connect(connection *websocket.Conn) {
 			delete(clients, connection)
 			break
 		}
-		fmt.Printf("Received Message From %s \n - Message: %s", clients[connection], message)
+		fmt.Printf("Received Message From %s \n - Message: %s\n", clients[connection], message)
 
 		message = fmt.Sprintf("%s: %s", clients[connection], message)
 
